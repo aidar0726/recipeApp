ae63ccab7bc1fbdf2743db0776f55bb05c9f4077
shakirov_a <Shakirov0726@gmail.com>
2023-01-30 20:20:51 +0600

    проект рецепты (добавил необходимые библиотеки(Apache Commons,Swagger) использовал их в коде.

--- src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -2,6 +2,8 @@ package com.example.recipeapp.controllers;
 
 import com.example.recipeapp.model.Recipe;
 import com.example.recipeapp.services.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты",description = "CRUD-операции для работы с рецептами")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -17,12 +20,18 @@ public class RecipeController {
     }
 
     @PostMapping
+    @Operation (
+            summary = "Добавляет рецепт"
+    )
     public ResponseEntity<Integer> addRecipe(@RequestBody Recipe recipe) {
         Integer id = recipeService.addRecipe(recipe);
         return ResponseEntity.ok().body(id);
     }
 
     @GetMapping("/{id}")
+    @Operation (
+            summary = "Ищет рецепт по id"
+    )
     public ResponseEntity<Recipe> getRecipeById(@PathVariable Integer id) {
         Recipe recipe = recipeService.getRecipe(id);
         if (recipe == null) {
@@ -33,12 +42,18 @@ public class RecipeController {
     }
 
     @GetMapping("/ingredient/{value}")
+    @Operation (
+            summary = "Ищет рецепты по ингредиенту"
+    )
     public ResponseEntity<Map<Integer, Recipe>> getRecipeByIngredient(@PathVariable String value) {
         Map<Integer, Recipe> recipes = recipeService.getRecipeByIngredient(value);
         return ResponseEntity.ok(recipes);
     }
 
     @PutMapping("/{id}")
+    @Operation (
+            summary = "Редактирует рецепт по id"
+    )
     public ResponseEntity<Recipe> editRecipe(@PathVariable Integer id, @RequestBody Recipe recipe) {
         Recipe modifiedRecipe = recipeService.editRecipe(id, recipe);
         if (modifiedRecipe == null) {
@@ -48,6 +63,9 @@ public class RecipeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation (
+            summary = "Удаляет рецепт по id"
+    )
     public ResponseEntity<Void> deleteRecipe(@PathVariable Integer id) {
         if (recipeService.deleteRecipe(id)) {
             return ResponseEntity.ok().build();
@@ -56,6 +74,9 @@ public class RecipeController {
     }
 
     @GetMapping
+    @Operation (
+            summary = "Показывает все рецепты"
+    )
     public ResponseEntity<Map<Integer, Recipe>> getAllRecipe() {
         Map<Integer, Recipe> recipes = recipeService.getAllRecipe();
         return ResponseEntity.ok(recipes);
--- src/main/java/com/example/recipeapp/model/validation/Validation.java
+++ src/main/java/com/example/recipeapp/model/validation/Validation.java
@@ -1,11 +1,13 @@
 package com.example.recipeapp.model.validation;
 
 import com.example.recipeapp.model.exceptions.ValidationException;
+import org.apache.commons.lang3.StringUtils;
 
 public class Validation {
 
     public static String validateString(String value) throws ValidationException {
-        if (value == null || value.isBlank() || value.isEmpty()) {
+        //if (value == null || value.isBlank() || value.isEmpty())
+        if (StringUtils.isBlank(value)) {
             throw new ValidationException("Введите корректные данные!");
         } else {
             return value;
